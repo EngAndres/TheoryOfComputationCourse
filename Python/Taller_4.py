73292bbe5be378ab1cc0ea2181f49304127dbf1c
EngAndres <jerarquia04@gmail.com>
2017-04-07 20:52:18 -0500

    Taller 4 - Python

---
+++ Python/Taller_4.py
@@ -0,0 +1,36 @@
+input = raw_input("")
+accepted = True
+
+size = len(input)
+index = 0
+
+#starts in q0
+if(index < size and input[index]=='b'):
+    #move to q1
+    index += 1
+    
+    while(index < size and input[index]=='b'):
+        #loop in q1
+        index += 1
+    
+    if(index < size and input[index]=='a'):
+        #move to q2
+        index += 1
+        
+        while(index < size and input[index]=='b'):
+            #loop in q2
+            index += 1
+        
+    else:
+        accepted = False
+else:
+    accepted = False
+
+if index != size:
+    accepted = False
+
+
+if accepted:
+    print("Cadena Aceptada")
+else:
+    print("Cadena Rechazada")
